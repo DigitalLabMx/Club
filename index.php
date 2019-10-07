c6ead47a975f2e8c656cf124cdce891e50ed04f0
DigitalLabMx <56239632+DigitalLabMx@users.noreply.github.com>
2019-10-06 20:08:17 -0500

    Plantilla de antro

---
+++ index.php
@@ -0,0 +1,25 @@
+ <!DOCTYPE html>
+<html>
+<head>
+    <title>Digital Lab Nightclub</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Página del mejor club de la ciudad de México">
+    <meta name="keywords" content="club, diversión, ciudad de México">
+    <link rel="stylesheet" href="css/navbar.css"> 
+</head>
+<body>
+
+    <!--VIDEO-->
+        <?php include("php/introVideo.php"); ?> 
+    <!--VIDEO-->
+
+    <!--NAV BAR-->  
+    <div style="position: fixed; top: 0; width: 100%">
+        <?php include("php/navbar.php"); ?> 
+    </div>
+     <!--NAV BAR-->    
+
+</body>
+
+</html> 
---
+++ php/introVideo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    
+</head>
+<body>
+
+    <video autoplay muted loop style="position: fixed; right: 0; bottom: 0; min-width: 100%; min-height: 100%">
+            <source src="videos/intro_club_video.mp4" type="video/mp4">
+            Tu navegador no soporta video HTML5.
+    </video> 
+            
+
+</body>
+
+</html> 
